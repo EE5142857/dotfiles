149cf31d940094723dfe93e6222cb17648ef8b39
foo <foo@bar.com>
2022-03-05 00:35:05 +0900

    Changes to be committed: 	deleted:    bin/create_env_win/create_env.bat 	deleted:    bin/create_env_win/create_env_vscode.bat 	modified:   bin/create_env_win/scoop.ps1
    Untracked files:
    	bin/create_env_win/setup_vim.bat
    	bin/create_env_win/setup_vscode.bat

--- bin/create_env_win/scoop.ps1
+++ bin/create_env_win/scoop.ps1
@@ -1,13 +1,21 @@
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 iwr -useb get.scoop.sh | iex
 
+Remove-item $env:USERPROFILE\.gitconfig
 scoop install git
-
-scoop bucket add extras
-
 scoop install neovim
 scoop install vim
+
+mkdir C:\work -Force
+cd C:\work
+git clone https://github.com/EE5142857/dotfiles
+
+scoop bucket add extras
 scoop install vscode
 
+Start-Process C:\work\dotfiles\bin\create_env_win\setup_vim.bat
+Start-Process C:\work\dotfiles\bin\create_env_win\setup_vscode.bat
+
+$host.UI.RawUI.ReadKey()
 # scoop uninstall scoop
 # del .\scoop -Force
