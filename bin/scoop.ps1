e62ba66f43baf48e37e4c17bd5f1d20048536058
foo <foo@bar.com>
2022-03-17 11:38:37 +0900

    Changes to be committed: 	modified:   bin/scoop.ps1

--- bin/scoop.ps1
+++ bin/scoop.ps1
@@ -1,10 +1,40 @@
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
-iwr -useb get.scoop.sh | iex
+if (!(Get-Command * | Where-object { $_.Name -match "scoop" })) {
+    iwr -useb get.scoop.sh | iex
+}
 
+# main
+scoop install 7zip
 scoop install git
 scoop install neovim
 scoop install vim
+scoop install busybox
+scoop install ctags
+scoop install postgresql
+scoop install r
 
+# solve "scoop checkup" problems
+scoop install innounp
+scoop install dark
+
+# extras
 scoop bucket add extras
-scoop install vscode
 scoop install deno
+scoop install everything
+scoop install vscode
+scoop install winmerge
+
+# versions
+scoop bucket add versions
+scoop install python39
+
+# jetbrains
+scoop bucket add jetbrains
+
+# maintenance
+scoop update
+scoop update *
+scoop cleanup *
+scoop checkup
+
+pause
