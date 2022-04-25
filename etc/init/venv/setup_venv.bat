b92315a612c2149a4dce0013427ef00ec8287e6c
foo <foo@bar.com>
2022-04-25 10:15:59 +0900

    Changes to be committed: 	modified:   etc/init/venv/setup_venv.bat

--- etc/init/venv/setup_venv.bat
+++ etc/init/venv/setup_venv.bat
@@ -1,29 +1,20 @@
 @echo off
 
 set PYTHON="python39"
-set DIR_PATH="C:\work"
-set ENV_NAME="myenv"
+set ENV_PATH="C:\work\myenv"
 
 :main
-    cd /d %DIR_PATH%
+    cd /d %~dp0
 
-    if not exist %ENV_NAME% (
-        %PYTHON% -m venv %ENV_NAME%
+    if not exist %ENV_PATH% (
+        %PYTHON% -m venv %ENV_PATH%
     )
 
-    call %ENV_NAME%\Scripts\activate.bat
+    call %ENV_PATH%\Scripts\activate.bat
     pip config --site set global.trusted-host "pypi.org pypi.python.org files.pythonhosted.org"
     pip install -U pip
-
-    for /f %%i in (%~dp0\requirements.txt) do (
-        call :pip_install "%%i"
-    )
+    pip install -r requirements.txt
 
     echo Done.
     pause
-exit /b
-
-@REM :pip_install %module%
-:pip_install
-    pip install %~1
 exit /b
