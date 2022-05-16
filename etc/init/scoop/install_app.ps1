28b6a120874176dde3be9a0bd543a2c83fd03115
EE5142857 <44958756+EE5142857@users.noreply.github.com>
2022-05-16 17:05:29 +0900

    modified

--- etc/init/scoop/install_app.ps1
+++ etc/init/scoop/install_app.ps1
@@ -1,45 +1,24 @@
 function global:Install-App ([bool]$do_full_install)
 {
     # main
-    # scoop install 7zip
-    # scoop install git
-    # scoop install dark
-    # scoop install innounp
-
-    # scoop install neovim
-    # scoop install nodejs
-    # scoop install postgresql
-    # scoop install r
-    # scoop install vim
-    scoop install gcc
-    scoop install ripgrep
-    # if ($do_full_install -eq $true) {
-    #     scoop install pandoc
-    # }
+    scoop install 7zip
+    scoop install grep
 
     # extras
     scoop bucket add extras
-    # scoop install anaconda3
     scoop install deno
-    # scoop install everything
-    # scoop install googlechrome
-    scoop install plantuml
-    # scoop install vscode
-    # scoop install winmerge
-    # if ($do_full_install -eq $true) {
-    #     scoop install android-studio
-    # }
+    if ($do_full_install -eq $true) {
+        scoop install plantuml
+    }
 
     # versions
     # scoop bucket add versions
-    # scoop install python39
 
     # java
-    # scoop bucket add java
-    # scoop install openjdk
-
-    # reset
-    # scoop reset python39
+    if ($do_full_install -eq $true) {
+        scoop bucket add java
+        scoop install openjdk
+    }
 
     # maintenance
     scoop update
