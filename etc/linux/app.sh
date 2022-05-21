109547bff24e863c68bc77be911b90bbb636f625
foo <foo@bar.com>
2022-05-22 03:38:16 +0900

    Changes to be committed: 	renamed:    etc/linux/init_vim.sh -> etc/linux/app.sh 	deleted:    etc/linux/apt.sh

--- etc/linux/app.sh
+++ etc/linux/app.sh
@@ -2,6 +2,9 @@
 # sudo vi /etc/apt/sources.list
 # :%s/# deb-src/deb-src
 sudo apt update
+sudo apt upgrade
+sudo apt intall zip
+curl -fsSL https://deno.land/install.sh | bash
 sudo apt build-dep vim
 sudo apt install build-essential
 sudo apt install python3-dev
--- etc/linux/apt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt upgrade
-sudo apt intall zip
-curl -fsSL https://deno.land/install.sh | sh
