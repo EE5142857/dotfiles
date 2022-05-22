fe62d3c7bf24e1ee334ad4edf0ae2e4139c0cbd4
foo <foo@bar.com>
2022-05-22 18:25:30 +0900

    Changes to be committed: 	modified:   etc/linux_init/symlink.sh

--- etc/linux_init/symlink.sh
+++ etc/linux_init/symlink.sh
@@ -3,13 +3,13 @@ set -euo pipefail
 cd `dirname $0`
 if [ -n "$(which wslpath)" ]; then
   ln -sd /mnt/c/work ~/work
-  sudo ln -s ~/dotfiles/wsl.conf /etc/wsl.conf
+  sudo ln -s ~/work/dotfiles/wsl.conf /etc/wsl.conf
 fi
-ln -sd ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -sd ~/work/dotfiles/.vim ~/.vim
+ln -s ~/work/dotfiles/.vimrc ~/.vimrc
 CONFDIR=~/.config
 if [ ! -e ${CONFDIR} ]; then
   mkdir ${CONFDIR}
 fi
-ln -sd ~/dotfiles/.vim ${CONFDIR}/nvim
-ln -s ~/dotfiles/.gitignore ~/.gitignore
+ln -sd ~/work/dotfiles/.vim ${CONFDIR}/nvim
+ln -s ~/work/dotfiles/.gitignore ~/.gitignore
