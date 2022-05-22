a572a6c75abfdffeb96b13a2f606ae16eb445836
foo <foo@bar.com>
2022-05-22 15:32:04 +0900

    Changes to be committed: 	modified:   etc/linux_init/app.sh 	modified:   etc/linux_init/vim_init.sh

--- etc/linux_init/vim_init.sh
+++ etc/linux_init/vim_init.sh
@@ -7,7 +7,7 @@ curl -fsSL https://deno.land/install.sh | bash
 echo "export DENO_INSTALL=\"~/.deno\"" >> ~/.profile
 echo "PATH=\"\$DENO_INSTALL/bin:\$PATH\"" >> ~/.profile
 source ~/.profile
-sudo vim /etc/apt/sources.list -c ":%s/# deb-src/deb-src | wq"
+sudo vim /etc/apt/sources.list -c "%s/# deb-src/deb-src" -c "wq"
 sudo apt -y build-dep vim
 sudo apt -y install build-essential
 sudo apt -y install python3-dev
