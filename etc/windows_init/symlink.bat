251cd1193bc64c9c606e6f8c2c16759aebb24390
EE5142857 <44958756+EE5142857@users.noreply.github.com>
2022-05-24 15:09:32 +0900

    modified

--- etc/windows_init/symlink.bat
+++ etc/windows_init/symlink.bat
@@ -6,11 +6,11 @@
     mkdir "%APPDATA%\Code\User"
 
     cd /d %~dp0..\..
-    mklink      "%USERPROFILE%\.vimrc"              "%cd%\.vimrc"
-    mklink      "%USERPROFILE%\_vimrc"              "%cd%\.vimrc"
-    mklink /d   "%USERPROFILE%\.vim"                "%cd%\.vim"
-    mklink /d   "%USERPROFILE%\vimfiles"            "%cd%\.vim"
-    mklink /d   "%LOCALAPPDATA%\nvim"               "%cd%\.vim"
+    @REM mklink      "%USERPROFILE%\.vimrc"              "%cd%\.vimrc"
+    @REM mklink      "%USERPROFILE%\_vimrc"              "%cd%\.vimrc"
+    @REM mklink /d   "%USERPROFILE%\.vim"                "%cd%\.vim"
+    @REM mklink /d   "%USERPROFILE%\vimfiles"            "%cd%\.vim"
+    @REM mklink /d   "%LOCALAPPDATA%\nvim"               "%cd%\.vim"
     mklink      "%APPDATA%\Code\User\settings.json" "%cd%\.vscode\settings.json"
     mklink /d   "%APPDATA%\Code\User\snippets"      "%cd%\.vim\vsnip"
     mklink      "%USERPROFILE%\.gitignore"          "%cd%\.gitignore"
