01ad5c330296135b2b3365966fc1bdada9daee3e
EE5142857 <44958756+EE5142857@users.noreply.github.com>
2022-05-25 00:33:27 +0900

    modified

--- etc/linux_init/vim_init.sh
+++ etc/linux_init/vim_init.sh
@@ -6,7 +6,7 @@ sudo apt -y install zip
 if [ -z "$(which deno)" ]; then
   curl -fsSL https://deno.land/install.sh | bash
   echo "export DENO_INSTALL=\"\${HOME}/.deno\"" >> ~/.profile
-  echo "PATH=\"\${DENO_INSTALL}/bin:\${PATH}\"" >> ~/.profile
+  echo "export PATH=\"\${DENO_INSTALL}/bin:\${PATH}\"" >> ~/.profile
 fi
 # sudo vim /etc/apt/sources.list -c "%s/# deb-src/deb-src" -c "wq"
 # sudo apt update
