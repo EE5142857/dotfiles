17f0f65d880956a7ad5453892f84b6a40a131acf
EE5142857 <44958756+EE5142857@users.noreply.github.com>
2022-05-27 01:03:26 +0900

    modified

--- etc/linux_init/clean.sh
+++ etc/linux_init/clean.sh
@@ -6,6 +6,7 @@ unlink ${HOME}/.vim
 unlink ${HOME}/.vimrc
 unlink ${HOME}/.config/nvim
 unlink ${HOME}/.gitignore
+unlink ${HOME}/.my_bashrc
 rm -rf ${HOME}/.cache
 rm -rf ${HOME}/.viminfo
 if [ -n "$(which wslpath)" ]; then
