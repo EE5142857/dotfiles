bda1156be138fa5f42203879585233aa46a6b02b
EE5142857 <44958756+EE5142857@users.noreply.github.com>
2022-05-27 00:57:39 +0900

    modified

--- etc/linux_init/init.sh
+++ etc/linux_init/init.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# cd
-# git clone --depth 1 https://github.com/EE5142857/dotfiles
-# cd /mnt/c/work
-# sudo git clone --depth 1 https://github.com/EE5142857/dotfiles
 set -euo pipefail
 cd `dirname $0`
 bash clean.sh
@@ -10,8 +6,8 @@ bash git.sh
 bash app.sh
 bash vim_init.sh
 bash vim_build.sh
-# bash nvim_init.sh
-# bash nvim_build.sh
+bash nvim_init.sh
+bash nvim_build.sh
 bash python310_init.sh
 bash python310_venv_myenv.sh
 bash clean.sh
