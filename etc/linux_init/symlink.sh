79884557032b4b7ba0decc873ecf66bdc61fb167
EE5142857 <44958756+EE5142857@users.noreply.github.com>
2022-05-27 10:50:17 +0900

    modified

--- etc/linux_init/symlink.sh
+++ etc/linux_init/symlink.sh
@@ -2,13 +2,14 @@
 set -euo pipefail
 cd `dirname $0`
 if [ -n "$(which wslpath)" ]; then
-  ln -sd /mnt/c/work ${HOME}/work
+  ln -s /mnt/c/work ${HOME}/work
   sudo ln -s /mnt/c/work/dotfiles/wsl.conf /etc/wsl.conf
 fi
-ln -sd /mnt/c/work/dotfiles/.vim ${HOME}/.vim
+ln -s /mnt/c/work/dotfiles/.vim ${HOME}/.vim
 ln -s /mnt/c/work/dotfiles/.vimrc ${HOME}/.vimrc
 CONFDIR="${HOME}/.config"
 mkdir -p ${CONFDIR}
-ln -sd /mnt/c/work/dotfiles/.vim ${CONFDIR}/nvim
+ln -s /mnt/c/work/dotfiles/.vim ${CONFDIR}/nvim
 ln -s /mnt/c/work/dotfiles/.gitignore ${HOME}/.gitignore
 ln -s /mnt/c/work/dotfiles/.bashrc ${HOME}/.my_bashrc
+ln -s /mnt/c/work/dotfiles/jupytext.yml ${HOME}/jupytext.yml
