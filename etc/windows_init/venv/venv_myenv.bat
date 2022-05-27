fa7e7f0ecab9dd6c690b63a584d4a91ea4c587b3
EE5142857 <44958756+EE5142857@users.noreply.github.com>
2022-05-27 09:41:30 +0900

    modified

--- etc/windows_init/venv/venv_myenv.bat
+++ etc/windows_init/venv/venv_myenv.bat
@@ -1,20 +1,20 @@
 @echo off
 
-set PYTHON_VERSION=3.10
+set PY_VER=3.10
 set ENV_PATH="C:\work\win_venv\myenv"
 
 :main
     cd /d %~dp0
 
     if not exist %ENV_PATH% (
-        py -%PYTHON_VERSION% -m venv %ENV_PATH%
+        py -%PY_VER% -m venv %ENV_PATH%
     )
 
     call %ENV_PATH%\Scripts\activate.bat
-    pip3 config --site set global.trusted-host "pypi.org pypi.python.org files.pythonhosted.org"
-    pip3 install -U pip
-    pip3 install wheel
-    pip3 install -r requirements.txt
+    pip config --site set global.trusted-host "pypi.org pypi.python.org files.pythonhosted.org"
+    pip install -U pip
+    pip install wheel
+    pip install -r requirements.txt
 
     echo Done.
     pause
