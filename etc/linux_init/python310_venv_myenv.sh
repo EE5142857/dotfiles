cf86860f84bf4d1c68a086d23f8481428fd8362c
EE5142857 <44958756+EE5142857@users.noreply.github.com>
2022-05-27 09:36:17 +0900

    modified

--- etc/linux_init/python310_venv_myenv.sh
+++ etc/linux_init/python310_venv_myenv.sh
@@ -5,8 +5,8 @@ ENV_PATH="${HOME}/work/venv/myenv"
 mkdir -p "${HOME}/work/venv"
 python3.10 -m venv ${ENV_PATH}
 . ${ENV_PATH}/bin/activate
-# pip3 config --site set global.trusted-host "pypi.org pypi.python.org files.pythonhosted.org"
-pip install -U pip3
-pip3 install wheel
-pip3 install -r requirements.txt
+# pip config --site set global.trusted-host "pypi.org pypi.python.org files.pythonhosted.org"
+pip install -U pip
+pip install wheel
+pip install -r requirements.txt
 deactivate
