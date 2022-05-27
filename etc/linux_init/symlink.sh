0f2c85010021397b4a230c5d1f0fdafce77bd360
EE5142857 <44958756+EE5142857@users.noreply.github.com>
2022-05-27 10:42:14 +0900

    modified

--- etc/linux_init/symlink.sh
+++ etc/linux_init/symlink.sh
@@ -3,12 +3,12 @@ set -euo pipefail
 cd `dirname $0`
 if [ -n "$(which wslpath)" ]; then
   ln -sd /mnt/c/work ${HOME}/work
-  sudo ln -s ${HOME}/work/dotfiles/wsl.conf /etc/wsl.conf
+  sudo ln -s /mnt/c/work/dotfiles/wsl.conf /etc/wsl.conf
 fi
-ln -sd ${HOME}/work/dotfiles/.vim ${HOME}/.vim
-ln -s ${HOME}/work/dotfiles/.vimrc ${HOME}/.vimrc
+ln -sd /mnt/c/work/dotfiles/.vim ${HOME}/.vim
+ln -s /mnt/c/work/dotfiles/.vimrc ${HOME}/.vimrc
 CONFDIR="${HOME}/.config"
 mkdir -p ${CONFDIR}
-ln -sd ${HOME}/work/dotfiles/.vim ${CONFDIR}/nvim
-ln -s ${HOME}/work/dotfiles/.gitignore ${HOME}/.gitignore
-ln -s ${HOME}/work/dotfiles/.bashrc ${HOME}/.my_bashrc
+ln -sd /mnt/c/work/dotfiles/.vim ${CONFDIR}/nvim
+ln -s /mnt/c/work/dotfiles/.gitignore ${HOME}/.gitignore
+ln -s /mnt/c/work/dotfiles/.bashrc ${HOME}/.my_bashrc
