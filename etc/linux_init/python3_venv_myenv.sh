ed8785e4e4c6e40b4582365363715fa8ddca9869
EE5142857 <44958756+EE5142857@users.noreply.github.com>
2022-05-27 09:42:10 +0900

    modified

--- etc/linux_init/python3_venv_myenv.sh
+++ etc/linux_init/python3_venv_myenv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 cd `dirname $0`
-PYTHON_VER=3.10
+PY_VER=3.10
 ENV_PATH="${HOME}/work/venv/myenv"
 mkdir -p "${HOME}/work/venv"
-python${PYTHON_VER} -m venv ${ENV_PATH}
+python${PY_VER} -m venv ${ENV_PATH}
 . ${ENV_PATH}/bin/activate
-# python${PYTHON_VER} -m pip config --site set global.trusted-host "pypi.org pypi.python.org files.pythonhosted.org"
-python${PYTHON_VER} -m pip install -U pip
-python${PYTHON_VER} -m pip install wheel
-python${PYTHON_VER} -m pip install -r requirements.txt
+# python${PY_VER} -m pip config --site set global.trusted-host "pypi.org pypi.python.org files.pythonhosted.org"
+python${PY_VER} -m pip install -U pip
+python${PY_VER} -m pip install wheel
+python${PY_VER} -m pip install -r requirements.txt
 deactivate
