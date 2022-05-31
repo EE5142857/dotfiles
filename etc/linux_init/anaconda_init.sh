663ff9c6a0633878355a49ec1acf03d885480d4f
foo <foo@bar.com>
2022-05-31 23:42:15 +0900

    Changes to be committed:

--- etc/linux_init/anaconda_init.sh
+++ etc/linux_init/anaconda_init.sh
@@ -6,6 +6,7 @@ sudo apt -y update
 cd "${HOME}" || exit
 wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
 bash Anaconda3-2022.05-Linux-x86_64.sh
+. ~/.bashrc
 conda config --append channels conda-forge
 conda config --set auto_activate_base false
 conda update --all
